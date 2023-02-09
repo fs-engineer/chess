18f66491ebf46fc67253ec173d2f2359ab726b9b
Vadym Yevlanov <vad.evlanov@gmail.com>
2023-02-09 14:49:34 +0100

    models added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
-import React from "react";
-import "./App.css";
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import BoardComponent from './components/BoardComponent';
+import { Board } from './models/Board';
 
 function App() {
-  return <div></div>;
+  const [board, setBoard] = useState(new Board());
+
+  useEffect(() => {
+    restart();
+  }, []);
+
+  function restart() {
+    const newBoard = new Board();
+    newBoard.initCells();
+    setBoard(newBoard);
+  }
+
+  return (
+    <div className="app">
+      <BoardComponent />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/CellComponent.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CellComponent = () => {
+  return <div></div>;
+};
+
+export default CellComponent;
